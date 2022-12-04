f6f03f40e218370b41c69875505ebfd74bae8235
JMQuehl <jannik.quehl@gmail.com>
2022-12-04 09:26:59 +0100

    Solved task 03-1 and added task 03-2 to description

--- advent_of_code_2022/tasks/task03/task03.py
+++ advent_of_code_2022/tasks/task03/task03.py
@@ -1,6 +1,11 @@
 from advent_of_code_2022.AdventOfCodeTemplate import AdventOfCodeProblem
 from typing import List
 
+
+def get_letter_value(letter: chr):
+    return ord(letter) - ord('A') + 27 if letter.isupper() else ord(letter) - ord('a') + 1
+
+
 class Task03(AdventOfCodeProblem):
     def __init__(self):
         super().__init__()
@@ -9,10 +14,21 @@ class Task03(AdventOfCodeProblem):
         self.task_number = 3
 
     def solve_task(self, input_file_content: List[str]):
-        return 0
+        current_sum = 0
+        for line in input_file_content:
+            line = line.replace('\n', '')
+            first_compartment = sorted(line[0:len(line) // 2])
+            second_compartment = sorted(line[len(line) // 2:])
+            letters_in_both = [x for x in first_compartment if x in second_compartment]
+            current_sum += sum(set([get_letter_value(x) for x in first_compartment if x in second_compartment]))
+        return current_sum
 
     def solve_bonus_task(self, input_file_content: List[str]):
         return 0
 
     def is_input_valid(self, input_file_content: List[str]):
+        for line in input_file_content:
+            pure_line = line.replace('\n', '')
+            if not pure_line.isalpha() or not len(line) % 2 != 0:
+                return False
         return True
--- tests/test_task03.py
+++ tests/test_task03.py
@@ -1,5 +1,6 @@
+import string
 import unittest
-from advent_of_code_2022.tasks.task03.task03 import Task03
+from advent_of_code_2022.tasks.task03.task03 import Task03, get_letter_value
 
 
 class MainTaskTests(unittest.TestCase):
@@ -12,4 +13,11 @@ class MainTaskTests(unittest.TestCase):
                    "CrZsJsPPZsGzwwsLwLmpwMDw\n"]
 
     def test_given_example(self):
-        assert self.task.solve_task(self.input_lines) == 157
+        assert self.task.solve_task(self.known_input) == 157
+
+    def test_letter_value(self):
+        expected_result = list(range(1, 52 + 1))
+        input_list = string.ascii_lowercase + string.ascii_uppercase
+        result = [get_letter_value(x) for x in input_list]
+        assert result == expected_result
+
