a926c1f7826385304bfdccf65a279e66a846828f
JMQuehl <jannik.quehl@gmail.com>
2022-12-16 21:39:01 +0100

    Solved part 1 of task 16.

--- advent_of_code_2022/tasks/task16/task16.py
+++ advent_of_code_2022/tasks/task16/task16.py
@@ -1,9 +1,26 @@
 import re
 from advent_of_code_2022.AdventOfCodeTemplate import AdventOfCodeProblem
-from typing import List, Tuple
+from typing import List, Dict
+from sortedcontainers import SortedList
+
+
+class Valve:
+    identifier: str
+    flow_rate: int
+    neighbors: List[str]
+
+    def __init__(self, identifier: str, flow_rate=0, neighbors=None):
+        if neighbors is None:
+            neighbors = []
+        self.identifier = identifier
+        self.flow_rate = flow_rate
+        self.neighbors = neighbors
 
 
 class Task16(AdventOfCodeProblem):
+    valves: Dict[str, Valve]
+    distance_maps: Dict[str, Dict]
+    interesting_valves: List[str]
 
     def __init__(self, args):
         super().__init__(args)
@@ -11,13 +28,55 @@ class Task16(AdventOfCodeProblem):
         self.bonus_answer_text = "%d"
         self.task_number = 16
 
+    def parse_input(self, input_file_content: List[str]):
+        self.valves = {}
+        self.interesting_valves = []
+        for line in input_file_content:
+            name_list = re.findall(r'[A-Z]{2}', line)
+            flow_rate = int(re.findall(r'\d+', line)[0])
+            valve = Valve(name_list[0], flow_rate, name_list[1:])
+            self.valves[valve.identifier] = valve
+            if valve.flow_rate > 0:
+                self.interesting_valves.append(valve.identifier)
+
+    def calculate_distance_map(self, valve_id: str):
+        dist_map = {valve_id: 0}
+        visited = [valve_id]
+        can_visit = SortedList([(1, x) for x in self.valves[valve_id].neighbors], key=lambda x: x[0])
+        while len(can_visit) > 0:
+            dist, current_valve = can_visit.pop(0)
+            dist_map[current_valve] = dist
+            for neighbor in [x for x in self.valves[current_valve].neighbors if x not in visited]:
+                can_visit.add((dist + 1, neighbor))
+            visited.append(current_valve)
+        self.distance_maps[valve_id] = dist_map
+
+    def calculate_distance_maps(self):
+        self.distance_maps = {}
+        self.calculate_distance_map('AA')
+        for valve_id in self.interesting_valves:
+            self.calculate_distance_map(valve_id)
+
+    def calc_max_flow(self, starting_valve: str, time_left: int, interesting_nodes_left: List[str]):
+        if time_left <= 0:
+            return 0
+        dist_map = self.distance_maps[starting_valve]
+        max_flow = 0
+        for target in interesting_nodes_left:
+            list_copy = list(interesting_nodes_left)
+            list_copy.remove(target)
+            max_flow = max(max_flow, self.calc_max_flow(target, time_left - dist_map[target] - 1, list_copy))
+        return max_flow + (time_left * self.valves[starting_valve].flow_rate)
+
     def solve_task(self, input_file_content: List[str]):
-        return 0
+        self.parse_input(input_file_content)
+        self.calculate_distance_maps()
+        return self.calc_max_flow('AA', 30, self.interesting_valves)
 
     def solve_bonus_task(self, input_file_content: List[str]):
         return 0
 
     def is_input_valid(self, input_file_content: List[str]):
         return all(
-            re.fullmatch("", line) for line in
-            input_file_content)
+            re.fullmatch("Valve [A-Z]{2} has flow rate=\d+; tunnels? leads? to valves? [A-Z]{2}(, [A-Z]{2})*\n?", line)
+            for line in input_file_content)
