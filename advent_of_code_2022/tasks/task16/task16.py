0e7c8d51807f43d618bbeb10891633ee1431141a
JMQuehl <jannik.quehl@gmail.com>
2022-12-16 16:50:00 +0100

    Added structure for task 16.

---
+++ advent_of_code_2022/tasks/task16/task16.py
@@ -0,0 +1,23 @@
+import re
+from advent_of_code_2022.AdventOfCodeTemplate import AdventOfCodeProblem
+from typing import List, Tuple
+
+
+class Task16(AdventOfCodeProblem):
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.answer_text = "You can release at most %d pressure."
+        self.bonus_answer_text = "%d"
+        self.task_number = 16
+
+    def solve_task(self, input_file_content: List[str]):
+        return 0
+
+    def solve_bonus_task(self, input_file_content: List[str]):
+        return 0
+
+    def is_input_valid(self, input_file_content: List[str]):
+        return all(
+            re.fullmatch("", line) for line in
+            input_file_content)
---
+++ tests/test_task16.py
@@ -0,0 +1,20 @@
+from tests.abstract_test import TaskTest
+import unittest
+from advent_of_code_2022.tasks.task16.task16 import Task16
+from advent_of_code_2022.advent_of_code_utils import parse_args
+
+
+class Task05Tests(TaskTest, unittest.TestCase):
+    task = Task16(parse_args(['--visualize']))
+    known_input = ["Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\n",
+                   "Valve BB has flow rate=13; tunnels lead to valves CC, AA\n",
+                   "Valve CC has flow rate=2; tunnels lead to valves DD, BB\n",
+                   "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE\n",
+                   "Valve EE has flow rate=3; tunnels lead to valves FF, DD\n",
+                   "Valve FF has flow rate=0; tunnels lead to valves EE, GG\n",
+                   "Valve GG has flow rate=0; tunnels lead to valves FF, HH\n",
+                   "Valve HH has flow rate=22; tunnel leads to valve GG\n",
+                   "Valve II has flow rate=0; tunnels lead to valves AA, JJ\n",
+                   "Valve JJ has flow rate=21; tunnel leads to valve II"]
+    known_output = 1651
+    known_bonus_output = 0
